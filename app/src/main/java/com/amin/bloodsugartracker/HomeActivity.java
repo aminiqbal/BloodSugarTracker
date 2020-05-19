6d13964f0bc07660686ba646d644bff0ee4cf9e7
Iqbal Amin <twosixfoursix@gmail.com>
2020-05-18 21:30:56 -0400

    Initial commit

---
+++ app/src/main/java/com/amin/bloodsugartracker/HomeActivity.java
@@ -0,0 +1,215 @@
+package com.amin.bloodsugartracker;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+
+// app icon shape src = http://clipart-library.com/clipart/BcaonGRc8.htm
+
+public class HomeActivity extends AppCompatActivity
+{
+	private ImageView addEntryButton;
+
+	private File f;
+	private File ff[];
+	private ArrayList<File> file_list;
+
+	private FileInputStream fis;
+
+	private RecyclerView rv;
+	private RecyclerView.Adapter rva;
+	private RecyclerView.LayoutManager rvlm;
+
+	private String entryDetail = "";
+	private String entryDetailProcessed[];
+
+	private Double glucoselvl = 0.0;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home_list);
+
+		addEntryButton = findViewById(R.id.add_entry);
+		addEntryButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				Intent newEntryIntent = new Intent(HomeActivity.this, NewEntryActivity.class);
+				startActivity(newEntryIntent);
+			}
+		});
+
+		file_list = new ArrayList<>();
+
+		f = new File(getFilesDir().toString());
+		ff = f.listFiles();
+
+		if(ff != null)
+		{
+			for(int i = 0; i < ff.length; ++i)
+			{
+				file_list.add(ff[i]);
+			}
+		}
+
+		rv = (RecyclerView) findViewById(R.id.entry_list);
+		rvlm = new LinearLayoutManager(this);
+		rv.setLayoutManager(rvlm);
+
+		rva = new la(file_list);
+		rv.addItemDecoration(new DividerItemDecoration(HomeActivity.this, DividerItemDecoration.VERTICAL));
+		rv.setAdapter(rva);
+	}
+
+	private class vh extends RecyclerView.ViewHolder
+	{
+		private TextView datetime;
+		private TextView glucoselevel;
+		private TextView status;
+		private Button entrydeletebutton;
+
+		public vh(LayoutInflater inf, ViewGroup parent)
+		{
+			super(inf.inflate(R.layout.list_item, parent, false));
+			glucoselevel = itemView.findViewById(R.id.blood_glucose_level);
+			status = itemView.findViewById(R.id.status_description);
+			datetime = itemView.findViewById(R.id.date_time);
+			entrydeletebutton = itemView.findViewById(R.id.delete_entry_button);
+			entrydeletebutton.setOnClickListener(new View.OnClickListener()
+			{
+				@Override
+				public void onClick(View v)
+				{
+					new AlertDialog.Builder(HomeActivity.this)
+							.setTitle("Confirm Deletion")
+							.setMessage("Do you want to delete this entry?")
+							.setNegativeButton(android.R.string.no, null)
+							.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener()
+							{
+								public void onClick(DialogInterface arg00, int arg01)
+								{
+									int pos = getLayoutPosition();
+									File fname = file_list.get(pos);
+
+									fname.delete();
+									Toast.makeText(HomeActivity.this, "Entry deleted.", Toast.LENGTH_SHORT).show();
+									recreate();
+								}
+							}).create().show();
+				}
+			});
+		}
+	}
+
+	public class la extends RecyclerView.Adapter<vh>
+	{
+		private ArrayList<File> items;
+
+		public la(ArrayList<File> _items)
+		{
+			items = _items;
+		}
+		@Override
+		public vh onCreateViewHolder(ViewGroup parent, int viewType)
+		{
+			LayoutInflater inf = LayoutInflater.from(parent.getContext());
+			return new vh(inf, parent);
+		}
+		@Override
+		public void onBindViewHolder(vh holder, int position)
+		{
+			try
+			{
+				fis = new FileInputStream(items.get(position).getAbsolutePath());
+				int r = fis.read();
+				StringBuilder builder = new StringBuilder();
+				while(r != -1)
+				{
+					builder.append((char) r);
+					r = fis.read();
+				}
+				fis.close();
+				entryDetail = builder.toString();
+				entryDetailProcessed = entryDetail.split("#");
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+
+			glucoselvl = Double.parseDouble(entryDetailProcessed[3]);
+
+			holder.glucoselevel.setText("Blood Glucose Level: " + entryDetailProcessed[3] + " mmol/L");
+			if(entryDetailProcessed[2].equals("FASTING"))
+			{
+				if(glucoselvl <= 7.0 && glucoselvl >= 4.0)
+				{
+					holder.glucoselevel.setTextColor(getResources().getColor(R.color.safe, getTheme()));
+				}
+				else
+				{
+					holder.glucoselevel.setTextColor(getResources().getColor(R.color.colorPrimary, getTheme()));
+				}
+			}
+//			else if(entryDetailProcessed[2].equals("BEFORE MEAL"))
+//			{
+//				if(glucoselvl <= 7.0 && glucoselvl >= 4.0)
+//				{
+//					holder.glucoselevel.setTextColor(getResources().getColor(R.color.safe, getTheme()));
+//				}
+//				else
+//				{
+//					holder.glucoselevel.setTextColor(getResources().getColor(R.color.colorPrimary, getTheme()));
+//				}
+//			}
+//			else if(entryDetailProcessed[2].equals("AFTER MEAL"))
+//			{
+//
+//			}
+			else if(entryDetailProcessed[2].equals("2 HOURS AFTER MEAL"))
+			{
+				if(glucoselvl < 8.5 && glucoselvl >= 4.0)
+				{
+					holder.glucoselevel.setTextColor(getResources().getColor(R.color.safe, getTheme()));
+				}
+				else
+				{
+					holder.glucoselevel.setTextColor(getResources().getColor(R.color.colorPrimary, getTheme()));
+				}
+			}
+			holder.status.setText("Condition: " + entryDetailProcessed[2]);
+			holder.datetime.setText("Time: " + entryDetailProcessed[0] + " @ " + entryDetailProcessed[1]);
+		}
+		@Override
+		public int getItemCount()
+		{
+			return items.size();
+		}
+	}
+
+	public void onRestart()
+	{
+		super.onRestart();
+		recreate();
+	}
+}
---
+++ app/src/main/java/com/amin/bloodsugartracker/NewEntryActivity.java
@@ -0,0 +1,256 @@
+package com.amin.bloodsugartracker;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.UUID;
+
+public class NewEntryActivity extends AppCompatActivity
+{
+	private ImageView discardEntryButton;
+	private Button saveEntryButton;
+
+	private DatePicker entryDate;
+	private int entryYear;
+	private int entryMonth;
+	private final String months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+	private int entryDay;
+	private String date;
+
+	private TimePicker entryTime;
+	private String time;
+	private int entryHour;
+	private int entryMinute;
+	private String ampmIndicator;
+
+	//private CheckBox entryFasting;
+	private RadioButton measurementCondition;
+	private String statusDesc = "NONE";
+
+	private EditText entryGlucoseLevel;
+
+	private File filename;
+	private FileOutputStream fos;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.bloodsugar_entry);
+
+		entryDate = findViewById(R.id.date_entry);
+		entryTime = findViewById(R.id.time_entry);
+		//entryFasting = findViewById(R.id.fasting_confirmation_entry);
+		entryGlucoseLevel = findViewById(R.id.blood_glucose_level_entry);
+
+		entryYear = entryDate.getYear();
+		entryMonth = entryDate.getMonth();
+		entryDay = entryDate.getDayOfMonth();
+
+		entryDate.init(entryYear, entryMonth, entryDay, new DatePicker.OnDateChangedListener()
+		{
+			@Override
+			public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+			{
+				entryYear = year;
+				entryMonth = monthOfYear;
+				entryDay = dayOfMonth;
+				date = entryDay + " " + months[entryMonth] + ", " + entryYear;
+			}
+		});
+
+		date = entryDay + " " + months[entryMonth] + ", " + entryYear;
+
+		entryHour = entryTime.getHour();
+		entryMinute = entryTime.getMinute();
+
+		entryTime.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener()
+		{
+			@Override
+			public void onTimeChanged(TimePicker view, int hourOfDay, int minute)
+			{
+				entryHour = hourOfDay;
+				entryMinute = minute;
+				if(entryHour > 12)
+				{
+					ampmIndicator = "PM";
+					entryHour -= 12;
+					if(entryHour < 0)
+					{
+						entryHour = -(entryHour);
+					}
+				}
+				else
+				{
+					ampmIndicator = "AM";
+				}
+
+				if(entryMinute < 10)
+				{
+					time = entryHour + ":0" + entryMinute + " " + ampmIndicator;
+				}
+				else
+				{
+					time = entryHour + ":" + entryMinute + " " + ampmIndicator;
+				}
+			}
+		});
+
+		if(entryHour > 12)
+		{
+			ampmIndicator = "PM";
+			entryHour -= 12;
+			if(entryHour < 0)
+			{
+				entryHour = -(entryHour);
+			}
+		}
+		else
+		{
+			ampmIndicator = "AM";
+		}
+
+		if(entryMinute < 10)
+		{
+			time = entryHour + ":0" + entryMinute + " " + ampmIndicator;
+		}
+		else
+		{
+			time = entryHour + ":" + entryMinute + " " + ampmIndicator;
+		}
+
+//		entryFasting.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+//		{
+//			@Override
+//			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+//			{
+//				if(isChecked)
+//				{
+//					fasting = "YES";
+//				}
+//				else
+//				{
+//					fasting = "NO";
+//				}
+//			}
+//		});
+
+		discardEntryButton = findViewById(R.id.discard_entry);
+		discardEntryButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				onBackPressed();
+			}
+		});
+
+		saveEntryButton = findViewById(R.id.save_entry);
+		saveEntryButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				if(!(entryGlucoseLevel.getText().toString().equals("")) && !(statusDesc.equals("NONE")))
+				{
+					new AlertDialog.Builder(NewEntryActivity.this)
+							.setTitle("Confirm Entry")
+							.setMessage("Entry Date: " + date + "\nEntry Time: " + time + "\nCondition: " + statusDesc + "\nBlood Glucose Level: " + Double.parseDouble(entryGlucoseLevel.getText().toString()) + " mmol/L")
+							.setNegativeButton(android.R.string.no, null)
+							.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener()
+							{
+								@Override
+								public void onClick(DialogInterface dialog, int which)
+								{
+									filename = new File(getFilesDir(), UUID.randomUUID().toString());
+									try
+									{
+										fos = openFileOutput(filename.getName(), Context.MODE_PRIVATE);
+										fos.write((date + "#" + time + "#" + statusDesc + "#" + Double.parseDouble(entryGlucoseLevel.getText().toString())).getBytes());
+										fos.close();
+										Toast.makeText(NewEntryActivity.this, "Entry has been created.", Toast.LENGTH_SHORT).show();
+										startActivity(new Intent(NewEntryActivity.this, HomeActivity.class));
+										finish();
+									}
+									catch(Exception e)
+									{
+										e.printStackTrace();
+									}
+								}
+							})
+							.create().show();
+				}
+				else
+				{
+					new AlertDialog.Builder(NewEntryActivity.this)
+							.setTitle("Missing Information")
+							.setMessage("Please review entry and fill in any missing fields.")
+							.setPositiveButton("OK", null)
+							.create().show();
+				}
+			}
+		});
+	}
+
+	public void onSelected(View v)
+	{
+		boolean selected = ((RadioButton)v).isChecked();
+		switch(v.getId())
+		{
+			case(R.id.in_fasting):
+				if(selected)
+				{
+					statusDesc = "FASTING";
+				}
+				break;
+//			case(R.id.in_before):
+//				if(selected)
+//				{
+//					statusDesc = "BEFORE MEAL";
+//				}
+//				break;
+//			case(R.id.in_after):
+//				if(selected)
+//				{
+//					statusDesc = "AFTER MEAL";
+//				}
+//				break;
+			case(R.id.in_twosafter):
+				if(selected)
+				{
+					statusDesc = "2 HOURS AFTER MEAL";
+				}
+				break;
+		}
+	}
+
+	@Override
+	public void onBackPressed()
+	{
+		new AlertDialog.Builder(NewEntryActivity.this)
+				.setTitle("Discard Entry").setMessage("Do you wish to discard this entry?")
+				.setNegativeButton(android.R.string.no, null)
+				.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener()
+				{
+					public void onClick(DialogInterface arg00, int arg01)
+					{
+						NewEntryActivity.super.onBackPressed();
+					}
+				}).create().show();
+	}
+}
